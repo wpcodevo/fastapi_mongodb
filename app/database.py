131427a4dd6d4ab160a072b406d27f25e2fbec4f
Edem <ziddah.edem@gmail.com>
2022-07-15 11:47:16 +0000

    updated

--- app/database.py
+++ app/database.py
@@ -2,8 +2,14 @@ from pymongo import mongo_client
 import pymongo
 from app.config import settings
 
-client = mongo_client.MongoClient(settings.DATABASE_URL)
-print('Connected to MongoDB...')
+client = mongo_client.MongoClient(
+    settings.DATABASE_URL, serverSelectionTimeoutMS=5000)
+
+try:
+    conn = client.server_info()
+    print(f'Connected to MongoDB {conn.get("version")}')
+except Exception:
+    print("Unable to connect to the MongoDB server.")
 
 db = client[settings.MONGO_INITDB_DATABASE]
 User = db.users
